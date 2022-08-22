5b34ee8d951c8d1b12c6dea4b8300dce62e33480
Lachesis <su_haonan@sina.com>
2022-08-22 16:03:34 +0800

    fix good wrist rotation

--- Assets/HandTrackingAnimation.cs
+++ Assets/HandTrackingAnimation.cs
@@ -118,17 +118,49 @@ public class HandTrackingAnimation : MonoBehaviour
             RightHandCtrl.localPosition = RightHandPos[20];
             RightHandCtrl.localRotation = RightHandRot[20];
             Vector3 LeftHandDelta = LeftHandJoints[0].GetTransform().parent.transform.position - LeftHandCtrl.position;
-            //Quaternion LeftHandRotDelta = LeftHandJoints[0].GetTransform().parent.transform.rotation * Quaternion.Inverse(LeftHandCtrl.rotation);
+            Vector3 LeftHandHorizon = LeftHandPos[4] - LeftHandPos[16];
+            Vector3 RightHandHorizon = RightHandPos[4] - RightHandPos[16];
             Vector3 RightHandDelta = RightHandJoints[0].GetTransform().parent.transform.position - RightHandCtrl.position; ;
-            //Quaternion RightHandRotDelta = RightHandJoints[0].GetTransform().parent.transform.rotation * Quaternion.Inverse(RightHandCtrl.rotation);
+
+            //LeftHandCtrl.position += LeftHandDelta;
+            //LeftHandCtrl.localRotation = LeftHandRot[20];
+            //RightHandCtrl.position += RightHandDelta;
+            //RightHandCtrl.localRotation = RightHandRot[20];
+            Vector3 LeftWristToMiddle = LeftHandPos[8] - LeftHandPos[20];
+            Vector3 RightWristToMiddle = RightHandPos[8] - RightHandPos[20];
+            Vector3 LeftWristForward = Vector3.Cross(LeftHandHorizon, LeftWristToMiddle);
+            Vector3 RightWristForward = Vector3.Cross(RightWristToMiddle, RightHandHorizon);
+            Quaternion rotation1 = Quaternion.LookRotation(LeftWristForward, LeftHand.up);
+            LeftHand.rotation = rotation1;
+            Quaternion rotation2 = Quaternion.LookRotation(RightWristForward, RightHand.up);
+            RightHand.rotation = rotation2;
+            //Vector3 forward = Vector3.Cross(transform.right, deltaPos);
+            //Quaternion rotation = Quaternion.LookRotation(forward, deltaPos);
+            //LeftHand.rotation = rotation;
+
             for (int i = 0; i < 20; i++)
             {
-                LeftHandJoints[i].UpdateRelativeTransfrom(LeftHandPos[i], LeftHandPos[i + 1], playerPos, LeftHandDelta);
+                if (i != 0)
+                {
+                    LeftHandJoints[i].UpdateRelativeTransfrom(LeftHandPos[i], LeftHandPos[i + 1], LeftHandPos[i - 1], LeftHandHorizon, playerPos, playerRot, LeftHandDelta, true);
+                }
+                else
+                {
+                    LeftHandJoints[i].UpdateRelativeTransfrom(LeftHandPos[i], LeftHandPos[i + 1], LeftHandPos[i], LeftHandHorizon, playerPos, playerRot, LeftHandDelta, true);
+                }
+
             }
 
             for (int i = 0; i < 20; i++)
             {
-                RightHandJoints[i].UpdateRelativeTransfrom(RightHandPos[i], RightHandPos[i + 1], playerPos, RightHandDelta);
+                if (i != 0)
+                {
+                    RightHandJoints[i].UpdateRelativeTransfrom(RightHandPos[i], RightHandPos[i + 1], LeftHandPos[i - 1], RightHandHorizon, playerPos, playerRot, RightHandDelta, false);
+                }
+                else
+                {
+                    RightHandJoints[i].UpdateRelativeTransfrom(RightHandPos[i], RightHandPos[i + 1], LeftHandPos[i], RightHandHorizon, playerPos, playerRot, RightHandDelta, false);
+                }
             }
         }
 
--- Assets/Scripts/HandJoint.cs
+++ Assets/Scripts/HandJoint.cs
@@ -24,22 +24,64 @@ public class HandJoint : MonoBehaviour
         return transform;
     }
 
-    public void UpdateRelativeTransfrom(Vector3 jointOriginPos, Vector3 ChildjointOriginPos, Vector3 playerPos, Vector3 handPosDelta)
+    public void UpdateRelativeTransfrom(Vector3 jointOriginPos, Vector3 ChildjointOriginPos, Vector3 ParentJointOriginPos, Vector3 Horizon, Vector3 playerPos, Quaternion playerRot, Vector3 handPosDelta, bool isLeft)
     {
         transform.localPosition = transform.parent.InverseTransformPoint(jointOriginPos + playerPos + handPosDelta);
 
-        if (GetComponentsInChildren<Transform>(true).Length > 1)
+        if (isLeft)
         {
-            Vector3 posTemp = transform.GetChild(0).position;
-            Quaternion rotTemp = transform.GetChild(0).rotation;
-            Vector3 deltaPos = ChildjointOriginPos - jointOriginPos;
-            Vector3 forward = Vector3.Cross(transform.right, deltaPos);
-            Quaternion rotation = Quaternion.LookRotation(forward, deltaPos);
-            transform.rotation = rotation;
-            //transform.up = posTemp - transform.position;
-            transform.GetChild(0).rotation = rotTemp;
-            transform.GetChild(0).position = posTemp;
+            if (GetComponentsInChildren<Transform>(true).Length > 1)
+            {
+                Vector3 posTemp = transform.GetChild(0).position;
+                Quaternion rotTemp = transform.GetChild(0).rotation;
+                Vector3 deltaPos = ChildjointOriginPos - jointOriginPos;
+                Vector3 forward = Vector3.Cross(Horizon, deltaPos);
+                Quaternion rotation = Quaternion.LookRotation(forward, deltaPos);
+                transform.rotation = rotation;
+                //Vector3 Finalforward = Vector3.Cross(transform.forward, transform.up);
+                //rotation = Quaternion.LookRotation(Finalforward, transform.up);
+                //transform.rotation = rotation;
+                //transform.up = posTemp - transform.position;
+                transform.GetChild(0).rotation = rotTemp;
+                transform.GetChild(0).position = posTemp;
+            }
+            else
+            {
+                Vector3 deltaPos = jointOriginPos - ParentJointOriginPos;
+                Vector3 forward = Vector3.Cross(Horizon, deltaPos);
+                Quaternion rotation = Quaternion.LookRotation(forward, deltaPos);
+                transform.rotation = rotation;
+                //Vector3 Finalforward = Vector3.Cross(transform.forward, transform.up);
+                //rotation = Quaternion.LookRotation(Finalforward, transform.up);
+                //transform.rotation = rotation;
+            }
         }
+        else
+        {
+            if (GetComponentsInChildren<Transform>(true).Length > 1)
+            {
+                Vector3 posTemp = transform.GetChild(0).position;
+                Quaternion rotTemp = transform.GetChild(0).rotation;
+                Vector3 deltaPos = ChildjointOriginPos - jointOriginPos;
+                Vector3 forward = Vector3.Cross(deltaPos, Horizon);
+                Quaternion rotation = Quaternion.LookRotation(forward, deltaPos);
+                transform.rotation = rotation;
+                //Vector3 Finalforward = Vector3.Cross(transform.forward, transform.up);
+                //rotation = Quaternion.LookRotation(Finalforward, transform.up);
+                //transform.rotation = rotation;
+                //transform.up = posTemp - transform.position;
+                transform.GetChild(0).rotation = rotTemp;
+                transform.GetChild(0).position = posTemp;
+            }
+            else
+            {
+                Vector3 deltaPos = jointOriginPos - ParentJointOriginPos;
+                Vector3 forward = Vector3.Cross(deltaPos, Horizon);
+                Quaternion rotation = Quaternion.LookRotation(forward, deltaPos);
+                transform.rotation = rotation;
+            }
+        }
+
 
 
         //transform.rotation = Quaternion.identity;
