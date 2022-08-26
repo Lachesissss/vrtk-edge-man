8520f5fa95161e03ed0e7d9aada9c595df0897a2
Lachesis <su_haonan@sina.com>
2022-08-26 19:47:08 +0800

    fix: through Multiplayer test, can't sync hand joints

--- Assets/Scripts/NetworkAvatar.cs
+++ Assets/Scripts/NetworkAvatar.cs
@@ -7,12 +7,12 @@ using UnityEngine.XR;
 
 public class NetworkAvatar : NetworkBehaviour
 {
-    public Transform AvatarTransform;
-    public Transform IKHead;
-    public Transform IKLeftTarget;
-    public Transform IKRightTarget;
-    public Transform IKLeftFoot;
-    public Transform IKRightFoot;
+    public Transform NetworkAvatarTransform;
+    public Transform NetworkIKHead;
+    public Transform NetworkIKLeftTarget;
+    public Transform NetworkIKRightTarget;
+    public Transform NetworkIKLeftFoot;
+    public Transform NetworkIKRightFoot;
     public Animator NetworkAnimator;
 
     private Transform LocalAvatarTransform;
@@ -22,7 +22,15 @@ public class NetworkAvatar : NetworkBehaviour
     private Transform LocalIKLeftFoot;
     private Transform LocalIKRightFoot;
     private Animator LocalAnimator;
+
     private GameObject LocalAvatar;
+
+    const int JOINTS_NUM = 20;
+    private HandJoint[] LocalLeftHandJoints = new HandJoint[JOINTS_NUM];
+    private HandJoint[] LocalRightHandJoints = new HandJoint[JOINTS_NUM];
+    private HandJoint[] NetworkLeftHandJoints = new HandJoint[JOINTS_NUM];
+    private HandJoint[] NetworkRightHandJoints = new HandJoint[JOINTS_NUM];
+
 
 
     public void MapTransform(Transform target, Transform rig)
@@ -37,18 +45,70 @@ public class NetworkAvatar : NetworkBehaviour
     {
         LocalAvatar = GameObject.FindGameObjectWithTag("LocalAvatar");
         LocalAvatarTransform = LocalAvatar.transform;
-        //LocalIKHead = GameObject.FindGameObjectWithTag("AvatarHead").transform;
-        //LocalIKLeftTarget = GameObject.FindGameObjectWithTag("AvatarLeftHand").transform;
-        //LocalIKRightTarget = GameObject.FindGameObjectWithTag("AvatarRightHand").transform;
-        //LocalIKLeftFoot = GameObject.FindGameObjectWithTag("AvatarLeftFoot").transform;
-        //LocalIKRightFoot = GameObject.FindGameObjectWithTag("AvatarRightFoot").transform;
+        LocalAnimator = LocalAvatar.GetComponent<Animator>();
         LocalIKHead = LocalAvatarTransform.Find("Armature/Hips/Spine/Spine1/Spine2/Neck/Head");
         LocalIKLeftTarget = LocalAvatarTransform.Find("Armature/Hips/Spine/Spine1/Spine2/LeftShoulder/LeftArm/LeftForeArm/LeftHand");
         LocalIKRightTarget = LocalAvatarTransform.Find("Armature/Hips/Spine/Spine1/Spine2/RightShoulder/RightArm/RightForeArm/RightHand");
         LocalIKLeftFoot = LocalAvatarTransform.Find("Armature/Hips/LeftUpLeg/LeftLeg/LeftFoot");
         LocalIKRightFoot = LocalAvatarTransform.Find("Armature/Hips/RightUpLeg/RightLeg/RightFoot");
-        NetworkAnimator = this.GetComponent<Animator>();
-        LocalAnimator = LocalAvatar.GetComponent<Animator>();
+
+        Transform currentLeft = LocalIKLeftTarget;
+        for (int i = 0; i < 5; i++)
+        {
+            currentLeft = LocalIKLeftTarget.GetChild(i);
+            LocalLeftHandJoints[i * 4] = currentLeft.GetComponent<HandJoint>();
+            for (int j = 1; j < 4; j++)
+            {
+                currentLeft = currentLeft.GetChild(0);
+                int num = i * 4 + j;
+                LocalLeftHandJoints[num] = currentLeft.GetComponent<HandJoint>();
+            }
+
+        }
+        Transform currentRight = LocalIKRightTarget;
+        for (int i = 0; i < 5; i++)
+        {
+            currentRight = LocalIKRightTarget.GetChild(i);
+            LocalRightHandJoints[i * 4] = currentRight.GetComponent<HandJoint>();
+            for (int j = 1; j < 4; j++)
+            {
+                currentRight = currentRight.GetChild(0);
+                int num = i * 4 + j;
+                LocalRightHandJoints[num] = currentRight.GetComponent<HandJoint>();
+            }
+
+        }
+
+        NetworkAnimator = GetComponent<Animator>();
+        Transform NetworkLeftHand = transform.Find("Armature/Hips/Spine/Spine1/Spine2/LeftShoulder/LeftArm/LeftForeArm/LeftHand");
+        Transform NetworkRightHand = transform.Find("Armature/Hips/Spine/Spine1/Spine2/RightShoulder/RightArm/RightForeArm/RightHand");
+
+        currentLeft = NetworkLeftHand;
+        for (int i = 0; i < 5; i++)
+        {
+            currentLeft = NetworkLeftHand.GetChild(i);
+            NetworkLeftHandJoints[i * 4] = currentLeft.GetComponent<HandJoint>();
+            for (int j = 1; j < 4; j++)
+            {
+                currentLeft = currentLeft.GetChild(0);
+                int num = i * 4 + j;
+                NetworkLeftHandJoints[num] = currentLeft.GetComponent<HandJoint>();
+            }
+
+        }
+        currentRight = NetworkRightHand;
+        for (int i = 0; i < 5; i++)
+        {
+            currentRight = NetworkRightHand.GetChild(i);
+            NetworkRightHandJoints[i * 4] = currentRight.GetComponent<HandJoint>();
+            for (int j = 1; j < 4; j++)
+            {
+                currentRight = currentRight.GetChild(0);
+                int num = i * 4 + j;
+                NetworkRightHandJoints[num] = currentRight.GetComponent<HandJoint>();
+            }
+
+        }
 
         if (isLocalPlayer)
         {
@@ -63,20 +123,24 @@ public class NetworkAvatar : NetworkBehaviour
     {
         if (isLocalPlayer)
         {
-            //MapTransform(AvatarTransform, LocalAvatarTransform);
-            //Debug.Log("AvatarTrans: " + AvatarTransform.position.ToString());
-            //Debug.Log("LocalAvatarTrans: " + LocalAvatarTransform.position.ToString());
-            MapTransform(IKHead, LocalIKHead);
-            MapTransform(IKLeftTarget, LocalIKLeftTarget);
-            MapTransform(IKRightTarget, LocalIKRightTarget);
-            IKRightTarget.position = LocalIKRightTarget.position;
-            //Debug.Log("IKRightHand: " + IKRightTarget.position.ToString());
-            //Debug.Log("AvatarRightHand: " + LocalIKRightTarget.position.ToString());
-            MapTransform(IKLeftFoot, LocalIKLeftFoot);
-            MapTransform(IKRightFoot, LocalIKRightFoot);
+            MapTransform(NetworkIKHead, LocalIKHead);
+            MapTransform(NetworkIKLeftTarget, LocalIKLeftTarget);
+            MapTransform(NetworkIKRightTarget, LocalIKRightTarget);
+            MapTransform(NetworkIKLeftFoot, LocalIKLeftFoot);
+            MapTransform(NetworkIKRightFoot, LocalIKRightFoot);
+
+            for (int i = 0; i < 20; i++)
+            {
+                MapTransform(NetworkLeftHandJoints[i].GetTransform(), LocalLeftHandJoints[i].GetTransform());
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                MapTransform(NetworkRightHandJoints[i].GetTransform(), LocalRightHandJoints[i].GetTransform());
+            }
 
             transform.position = LocalAvatar.transform.position;
             transform.forward = LocalAvatar.transform.forward;
+
             syncAnimator(NetworkAnimator, LocalAnimator);
         }
     }
